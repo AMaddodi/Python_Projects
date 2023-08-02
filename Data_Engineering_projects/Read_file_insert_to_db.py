b35e56f2d9ae47aa00cfb84a5ed3d6f8cf98a79f
Archana Maddodi <134722015+AMaddodi@users.noreply.github.com>
2023-08-02 14:09:02 +0100

    Update Read_file_insert_to_db.py

--- Data_Engineering_projects/Read_file_insert_to_db.py
+++ Data_Engineering_projects/Read_file_insert_to_db.py
@@ -8,7 +8,7 @@ def create_database():
     cur = conn.cursor()
     
     # create database 
-    cur.execute("DROP DATABASE accounts")
+    #cur.execute("DROP DATABASE accounts")
     cur.execute("CREATE DATABASE accounts")
     
     #close the connection
@@ -36,11 +36,13 @@ conn, cur = create_database()
 
 # create table
 accounts_table = ("""CREATE TABLE IF NOT EXISTS accountscountry(
-country_code VARCHAR PRIMARY KEY,
-short_name VARCHAR,
-table_name VARCHAR,
-long_name VARCHAR,
-currency_unit VARCHAR)
+Country VARCHAR PRIMARY KEY,
+Rank INT ,
+CCA3 VARCHAR,
+Capital VARCHAR,
+Continent VARCHAR,
+Population INT,
+World_Population_Percentage FLOAT);
 """)
 
 create_table(conn, cur, accounts_table)
@@ -48,15 +50,18 @@ create_table(conn, cur, accounts_table)
 # Insert data to table
 
 # read CSV file
-file_data = pd.read_csv('file.csv')
+file_data = pd.read_csv(r"C:\Users\Archana\Documents\DataAnalysis\python\world_population.csv")
+file_data = file_data[['Country', 'Rank', 'CCA3', 'Capital', 'Continent', '2022 Population', 'World Population Percentage']]
 
 accounts_data_query = ("""INSERT INTO accountscountry(
-country_code,
-short_name,
-table_name,
-long_name,
-currency_unit)
-VALUES (%s, %s, %s, %s, %s)
+Country,
+Rank,
+CCA3,
+Capital,
+Continent,
+Population,
+World_Population_Percentage)
+VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 insert_data(conn, cur, file_data, accounts_data_query)
